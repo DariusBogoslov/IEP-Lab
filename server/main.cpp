8acb23fdf4d82b06e40e6b07b91a22fc2bf0a3e8
DariusBogoslov <dariusbogoslov@yahoo.com>
2017-10-23 14:04:07 +0000

    finished lab 2

--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,26 @@
-#include <QCoreApplication>
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
-}
+    #include "pin.h"
+ 
+    int main(int argc, char *argv[])
+    {
+        pin *led = new pin(LED,OUTPUT);
+        led->setOutput();
+        pin *button = new pin(BUTTON,INPUT);
+        button->setInput();
+        button->setPullUp();
+        uint8_t readValue = -1;
+ 
+ 
+        while(1)
+        {
+            readValue = bcm2835_gpio_lev(button->numar);
+            led->set(0);
+            bcm2835_delay(500);
+            led->set(1);
+            bcm2835_delay(500);
+ 
+        }
+ 
+ 
+        bcm2835_close();
+        return 0;
+    }
---
+++ server/pin.cpp
@@ -0,0 +1,66 @@
+#include "pin.h"
+ 
+ 
+pin::pin(int numar,int direction)
+{
+    this->numar = numar;
+    this->direction = direction;
+    this ->state = 0;
+    this ->pullUp = 0;
+ 
+}
+ 
+ 
+void pin::set(int state)
+{
+    if(this->direction == 1 && state == 0)
+       {
+        bcm2835_gpio_write(this -> numar, LOW);
+        this ->state = 0;
+        }
+    else if(this->direction == 1 &&  state == 1)
+         {
+            bcm2835_gpio_write(this -> numar, HIGH);
+            this ->state = 1;
+        }
+ 
+}
+ 
+int pin::get()
+{
+    if(this -> direction == INPUT)
+    return  bcm2835_gpio_lev(this->numar);
+    else return -1;
+}
+ 
+void pin::changeDirection()
+{
+    if(this->direction == 0)
+    {
+        this ->direction = 1;
+        this->setOutput();
+    }
+    else{
+ 
+        this ->direction = 0;
+        this->setInput();
+    }
+}
+ 
+void pin::setOutput()
+{
+    bcm2835_gpio_fsel(this->numar, BCM2835_GPIO_FSEL_OUTP);
+    this -> direction = OUTPUT;
+}
+ 
+void pin::setInput()
+{
+    bcm2835_gpio_fsel(this->numar, BCM2835_GPIO_FSEL_INPT);
+    this -> direction = INPUT;
+}
+ 
+void pin::setPullUp()
+{
+    bcm2835_gpio_set_pud(this->numar, BCM2835_GPIO_PUD_UP);
+    this->pullUp = 1;
+}
